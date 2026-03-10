338cd2081d5d902970be32b3dccb3cc22ef76b14
Guido Doumen <git@doumen-laumen.de>
2026-03-11 00:07:16 +0100

    setup prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'avoid',
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    printWidth: 120,
+    tabWidth: 4,
 };
