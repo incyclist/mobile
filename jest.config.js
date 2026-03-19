83363db2729421ba4dcb4bff3dfe1d4f0156c7b8
Amos Burton <amos@agentic-scrum.ai>
2026-03-19 19:46:37 +0000

    fix(32): update tests and add svg mock

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
     '^react-native-linear-gradient$': '<rootDir>/__mocks__/react-native-linear-gradient.tsx',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.tsx',
   },
 };
