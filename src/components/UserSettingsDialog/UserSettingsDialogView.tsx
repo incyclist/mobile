65cb7e7a0c28fc49dce14f691808f7a7979cf0ce
Guido Doumen <git@doumen-laumen.de>
2026-03-20 11:59:22 +0100

    add padding

--- src/components/UserSettingsDialog/UserSettingsDialogView.tsx
+++ src/components/UserSettingsDialog/UserSettingsDialogView.tsx
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         paddingTop: 8,
+        paddingHorizontal: 16,
     },
     fields: {
         marginBottom: 16,
