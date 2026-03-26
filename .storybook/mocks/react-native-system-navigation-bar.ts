83bd91113c1faa81e1371865292210120495eaef
Amos Burton <amos@agentic-scrum.ai>
2026-03-26 11:55:43 +0000

    feat(95): hide android navigation bar in sticky immersive mode

---
+++ .storybook/mocks/react-native-system-navigation-bar.ts
@@ -0,0 +1,10 @@
+const SystemNavigationBar = {
+    stickyImmersive: () => Promise.resolve(),
+    immersive: () => Promise.resolve(),
+    leanBack: () => Promise.resolve(),
+    navigationHide: () => Promise.resolve(),
+    navigationShow: () => Promise.resolve(),
+    fullScreen: () => Promise.resolve(),
+};
+
+export default SystemNavigationBar;
