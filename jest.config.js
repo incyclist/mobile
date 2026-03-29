2a4532309d9818ec2c543b116d198148834c0cfe
Guido Doumen <git@doumen-laumen.de>
2026-03-29 11:54:45 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleNameMapper: {
     '^react-native-linear-gradient$': '<rootDir>/__mocks__/react-native-linear-gradient.tsx',
     '\\.svg$': '<rootDir>/__mocks__/svgMock.tsx',
+    'react-native-safe-area-context': '<rootDir>/__mocks__/react-native-safe-area-context.ts',
     '@react-navigation/native': '<rootDir>/src/__mocks__/@react-navigation/native.ts', // Added for ESM module support
   },
 };
