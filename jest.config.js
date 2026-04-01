69ef82d02ff85d38a2f8dda8d7a33277e9fbe60a
Guido Doumen <git@doumen-laumen.de>
2026-04-01 21:00:51 +0200

    add react-native-map mock for jest

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '__tests__/jest.setup.ts'],
   moduleNameMapper: {
     '^react-native-linear-gradient$': '<rootDir>/__mocks__/react-native-linear-gradient.tsx',
+    'react-native-maps': '<rootDir>/__mocks__/react-native-maps.tsx',
     '\\.svg$': '<rootDir>/__mocks__/svgMock.tsx',
     'react-native-safe-area-context': '<rootDir>/__mocks__/react-native-safe-area-context.ts',
     '@react-navigation/native': '<rootDir>/src/__mocks__/@react-navigation/native.ts', // Added for ESM module support
