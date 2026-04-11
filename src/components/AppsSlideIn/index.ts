afb13db08a8f2e9688cf56e7f1e342242a7eae02
Guido Doumen <git@doumen-laumen.de>
2026-04-11 17:36:34 +0200

    remove AppsSlideIn

--- src/components/AppsSlideIn/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './AppsSlideIn';
-export * from './types';
--- src/components/AppsSlideIn/types.ts
+++
@@ -1,9 +0,0 @@
-import { AppDisplayProps } from '../AppsSettings/types';
-
-export interface AppsSlideInProps {
-    visible: boolean;
-    apps?: AppDisplayProps[];
-    offsetX: number;
-    onSelect?: (key: string) => void;
-    onClose?: () => void;
-}
