0d825a2c77947b3be0c3ea986fbec3efbc9328f8
Guido Doumen <git@doumen-laumen.de>
2026-04-13 15:19:58 +0200

    fix missing rest Logs before app init

--- src/services/RestLogging/RestLogging.ts
+++ src/services/RestLogging/RestLogging.ts
@@ -6,6 +6,9 @@ import { getAppInfoBinding, getChannel} from "../../bindings/appInfo";
 import { getUserSettingsBinding } from "../../bindings/user-settings";
 
 
+const DEFAULT_LOG_URL = 'https://analytics.test.incyclist.com/api/v1'
+const DEFAULT_LOG_INTERVAL = 10
+
 const LOG_BLACKLIST = ['user', 'auth', 'cacheDir', 'baseDir', 'pageDir', 'appDir'];
 EventLogger.setKeyBlackList(LOG_BLACKLIST);
 
@@ -34,16 +37,17 @@ export const initRestLogging = async () => {
 
         await settings.getAll()
 
-        const logUrl = settings.get('logRest.url',undefined)
-        const sendInterval = settings.get('logRest.sendInterval',undefined)
+        const logUrl = settings.get('logRest.url',DEFAULT_LOG_URL )
+        const sendInterval = settings.get('logRest.sendInterval',DEFAULT_LOG_INTERVAL)
         const enabled = settings.get('logRest.enabled',true)
 
         const uuid = settings.get('uuid',undefined)
 
         if (enabled) {
 
-
-            apiConfig.addHeader('x-uuid',uuid)
+            if (uuid) {
+                apiConfig.addHeader('x-uuid',uuid)
+            }
             apiConfig.addHeader('x-platform',Platform.OS)
             apiConfig.addHeader('x-channel', getChannel())
             
