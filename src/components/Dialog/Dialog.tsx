a9bda7949d9d797dc3bb25c80fa5abc65bd9e0d4
Guido Doumen <git@doumen-laumen.de>
2026-09-10 12:03:03 +0200

    rollback previous change

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -376,6 +376,8 @@ const getStyles = ({ width, height, minWidth, minHeight, variant = 'details', is
             height: isCompact ? undefined : '100%',
             borderRadius: 0,
             maxHeight: '100%',
+            paddingLeft: safeAreaLeft,
+            paddingRight: safeAreaRight,
         },
     });
 };
